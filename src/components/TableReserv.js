808c60f2c669dcb2b4f723df45e83010bf1357e1
Wirkus Tomasz <wirkus919@gmail.com>
2019-11-28 10:56:17 +0100

    Several changes in reservationPage

--- src/components/TableReserv.js
+++ src/components/TableReserv.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import moment from 'moment'
 import { isInclusivelyAfterDay, SingleDatePicker} from 'react-dates';
-
+// import TimePicker from 'react-time-picker';
 
 class TableReserv extends React.Component{
     constructor(props) {
@@ -9,6 +9,7 @@ class TableReserv extends React.Component{
     
         this.state = {
             date: moment(),
+            time: '15:00',
             calendarFocused: false,
             numOfPeople: 0,
             phone: '',
@@ -28,20 +29,29 @@ class TableReserv extends React.Component{
       onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
       };
+      onTimeChange = ({time}) => {
+        this.setState(() => ({ time: time }));
+        console.log(typeof(time))
+      }
 
     render(){
         return(
             <div className="reservation__tableReserv">
                 <h2 className="heading-2">Zarezerwuj stolik</h2>
                 <SingleDatePicker
-                date={this.state.date}
-                onDateChange={this.onDateChange}
-                focused={this.state.calendarFocused}
-                onFocusChange={this.onFocusChange}
-                numberOfMonths={1}
-                isOutsideRange={day => !isInclusivelyAfterDay(day, moment())}
-
+                  date={this.state.date}
+                  onDateChange={this.onDateChange}
+                  focused={this.state.calendarFocused}
+                  onFocusChange={this.onFocusChange}
+                  numberOfMonths={1}
+                  isOutsideRange={day => !isInclusivelyAfterDay(day, moment())}
                 />
+                <input 
+                  type="time" min="10:00" max="22:00" 
+                  value={this.state.time} onChange={this.onTimeChange}
+                  className="reservation__time" 
+                />
+
             </div>
         )
        
@@ -51,3 +61,9 @@ class TableReserv extends React.Component{
 export default TableReserv
 
 
+// <TimePicker
+                //   onChange={this.onTimeChange}
+                //   value={this.state.time}
+                //   disableClock={true}
+                //   className="clock"
+                // />
